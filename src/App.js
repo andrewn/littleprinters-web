ddf97c805594de66494b37b8dc894a4263c2d400
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2019-03-17 22:24:38 +0100

    Poster text proof-of-concept

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import domtoimage from "dom-to-image";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+import Preview from "./Preview";
+
+const http = fetch; //(...params) => console.log("HTTP", ...params);
+
+const nord = {
+  printKey: "w6lswcvvn7icytjfdn46",
+  apiBase: "https://littleprinter.nordprojects.co"
+};
+
+const local = {
+  printKey: "s9njt909j6fbth8wkudb",
+  apiBase: "http://localhost:5000"
+};
+
+const config = local;
+
+function url() {
+  return `${config.apiBase}/printkey/${config.printKey}?from=thingy`;
+}
+
+function sendHtml(html) {
+  http(url(), {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      html
+    })
+  });
+}
+
+function sendImage(image) {
+  http(url(), {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "image/png"
+    },
+    body: image
+  });
+}
+
+async function generateImageFromPreview(ref) {
+  if (ref.current) {
+    console.log(ref.current);
+    return await domtoimage.toBlob(ref.current);
   }
+}
+
+function App() {
+  const [message, setMessage] = useState("This is a message!");
+  const [image, setImage] = useState();
+  const ref = React.createRef();
+
+  const doIt = async function() {
+    const image = await generateImageFromPreview(ref);
+    setImage(URL.createObjectURL(image));
+    sendImage(image);
+    // sendHtml(message);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={message}
+        onChange={evt => setMessage(evt.target.value)}
+      />
+      <Preview ref={ref} text={message} />
+      {image && <img src={image} alt="" />}
+      <button onClick={doIt}>Go</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Preview.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useLayoutEffect } from "react";
+
+import styles from "./Preview.module.css";
+
+export default React.forwardRef(function Preview({ text }, ref) {
+  useLayoutEffect(function() {
+    window.jQuery(ref.current).slabText();
+  });
+
+  return (
+    <div ref={ref} className={styles.container}>
+      {text}
+    </div>
+  );
+});
