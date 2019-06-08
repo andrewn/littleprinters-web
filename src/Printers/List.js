a56e6668ed53786fdf5f88e577c628771ccc6f59
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2019-06-08 11:15:04 +0200

    Blank slate

--- src/Printers/List.js
+++ src/Printers/List.js
@@ -6,24 +6,30 @@ import Printer from "./components/Printer";
 
 import styles from "./List.module.css";
 
+function PrinterList({ dispatch, printers }) {
+  return <ul className={`no-list ${styles.Printers}`}>
+    {Object.entries(printers).map(function ([id, printer]) {
+      return (
+        <li key={id}>
+          <Printer
+            id={id}
+            {...printer}
+            onDelete={() => deletePrinter(dispatch, id)}
+          />
+        </li>
+      );
+    })}
+  </ul>
+}
+
 export default function List({ state: { printers }, dispatch, }) {
+  const hasPrinters = Object.keys(printers).length > 0;
+
   return (
     <div className={styles.List}>
       <HeaderValues title="Little Printers" />
 
-      <ul className={`no-list`}>
-        {Object.entries(printers).map(function ([id, printer]) {
-          return (
-            <li key={id}>
-              <Printer
-                id={id}
-                {...printer}
-                onDelete={() => deletePrinter(dispatch, id)}
-              />
-            </li>
-          );
-        })}
-      </ul>
+      {hasPrinters && <PrinterList dispatch={dispatch} printers={printers} />}
 
       <div className={styles.Add}>
         <Button to="add">Add a printer</Button>
